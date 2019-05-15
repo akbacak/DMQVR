835ada2a282c1e581377071e2ca6b87bd194b798
akbacak <33653031+akbacak@users.noreply.github.com>
2019-05-15 16:54:01 +0300

    Add files via upload

---
+++ extractFrames.sh
@@ -0,0 +1,16 @@
+
+# Removes voice 
+ffmpeg -i Python/q1.mp4  -c copy -an Python/video_1.mp4
+ffmpeg -i Python/q2.mp4  -c copy -an Python/video_2.mp4
+
+# Extract frames
+ffmpeg -i Python/video_1.mp4 -vf fps=1 Python/Frames/video_1/video_1_%02d.jpg -hide_banner
+ffmpeg -i Python/video_2.mp4 -vf fps=1 Python/Frames/video_2/video_2_%02d.jpg -hide_banner
+
+
+# Resize for VGG16
+for f in "Python/Frames/video_*/*.jpg"
+do
+     mogrify $f -resize 224x224! $f
+done
+
--- predictVideoLabels.m
+++ predictVideoLabels.m
@@ -1,4 +1,7 @@
 
-system('sh  ./Python/extractFrames.sh');
 
-system('gnome-terminal  -- python Python/genNP.py');
+system('python /home/ubuntu/keras/enver/dmlvh2/DMQVR/Python/genNP.py');
+
+system('python /home/ubuntu/keras/enver/dmlvh2/DMQVR/Python/genHashCodes.py');
+
+system('python /home/ubuntu/keras/enver/dmlvh2/DMQVR/Python/genLabels.py');
