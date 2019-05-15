57fe2693b4bc2edeb03fc64be1d8330121514a0d
akbacak <33653031+akbacak@users.noreply.github.com>
2019-05-16 01:13:22 +0300

    Delete predictVideoLabels.m

--- Python/predictVideoLabels.m
+++
@@ -1,7 +0,0 @@
-
-
-system('python /home/ubuntu/keras/enver/dmlvh2/DMQVR/Python/genNP.py');
-
-system('python /home/ubuntu/keras/enver/dmlvh2/DMQVR/Python/genHashCodes.py');
-
-system('python /home/ubuntu/keras/enver/dmlvh2/DMQVR/Python/genLabels.py');
